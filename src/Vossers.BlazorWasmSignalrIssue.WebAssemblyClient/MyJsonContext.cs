f6ca57c143369ebb0a130fae62c5095172a15c9b
Jaap Vossers <jvossers@gmail.com>
2021-08-28 12:59:11 +0100

    source generator issue repro

---
+++ src/Vossers.BlazorWasmSignalrIssue.WebAssemblyClient/MyJsonContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using Vossers.BlazorWasmSignalrIssue.Shared;
+
+namespace Vossers.BlazorWasmSignalrIssue.WebAssemblyClient
+{
+
+    [JsonSerializable(typeof(ImmutableViewModel))]
+    internal partial class MyJsonContext : JsonSerializerContext
+    {
+
+    }
+}
